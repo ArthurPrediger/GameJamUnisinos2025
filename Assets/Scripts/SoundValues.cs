bce0bca48a45e763b3a26a7f185495890c4f3211
Filipe Patricio <filipeweschen@gmail.com>
2025-10-18 18:10:12 -0300

    MainMenu and SoundManager

---
+++ Assets/Scripts/SoundValues.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public enum SoundType
+{
+    //Put sound names in here
+}
+
+
+public class SoundValues : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

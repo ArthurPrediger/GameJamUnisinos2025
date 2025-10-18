3630e68c8ace0ecd553d7458ac9bcc3113c18bf2
Alexandre <alexandre.l.hoffmann@gmail.com>
2025-10-18 18:21:29 -0300

    Update animations character.

--- Assets/Scripts/GameplayScripts/PlayerMovement.cs
+++ Assets/Scripts/GameplayScripts/PlayerMovement.cs
@@ -16,10 +16,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform bulletSpawn;
     [SerializeField] private float shootingTime = 1f;
 
+    private Animator animator;
+
     private IInteractables itemInRange;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         rig = GetComponent<Rigidbody2D>();
     }
 
@@ -44,17 +47,28 @@ public class PlayerMovement : MonoBehaviour
         {
             movementX = -1;
             direction = Vector3.left;
+            animator.SetBool("IsWalking", true);
+            transform.localScale = new Vector3(-3, transform.localScale.y, transform.localScale.z);
 
         }
         else if (Input.GetKey(controllers.rightInput))
         {
             movementX = +1;
             direction = Vector3.right;
+            animator.SetBool("IsWalking", true);
+            transform.localScale = new Vector3(3, transform.localScale.y, transform.localScale.z);
 
         }
-        else if (Input.GetKey(controllers.rightInput) && Input.GetKey(controllers.leftInput)) movementX = 0;
-        else movementX = 0;
-
+        else if (Input.GetKey(controllers.rightInput) && Input.GetKey(controllers.leftInput))
+        {
+            movementX = 0;
+            animator.SetBool("IsWalking", false);
+        }
+        else
+        {
+            movementX = 0;
+            animator.SetBool("IsWalking", false);
+        }
         movementX = movementX * speed * Time.deltaTime;
         transform.position += new Vector3(movementX, 0, 0);
     }
@@ -66,6 +80,7 @@ public class PlayerMovement : MonoBehaviour
         {
             rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             isJumping = true;
+            animator.SetBool("IsJumping", true);
         }
 
     }
@@ -91,6 +106,7 @@ public class PlayerMovement : MonoBehaviour
         
     IEnumerator OnShootingTime()
     {
+        animator.SetTrigger("IsShooting");
         yield return new WaitForSeconds(shootingTime);
         isShooting = false;
     }
@@ -99,7 +115,8 @@ public class PlayerMovement : MonoBehaviour
             if(collision.gameObject.layer == 6)
             {
                 isJumping = false;
-            }
+            animator.SetBool("IsJumping", false);
+        }
         }
     private void OnTriggerEnter2D(Collider2D collision)
     {
